c77e368f153b0b5dc7d59f5d2c8d187d92c46345
Hasibur Rahman <hasiburdev@gmail.com>
2023-07-12 21:46:24 +0600

    Remove react shared lib

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,6 +1,7 @@
 // import styles from './page.module.scss';
 import fetch from 'node-fetch';
 import { Header } from '@eduocean/ui/shared';
+import { CourseList } from '@eduocean/web/features/course-list';
 
 const baseUrl = process.env.API_URL;
 
@@ -11,16 +12,19 @@ export default async function Index() {
    * Note: The corresponding styles are in the ./index.scss file.
    */
 
-  const response = await fetch(`${baseUrl}/users`);
-  const data = await response.json();
-  console.log(data);
+  // const response = await fetch(`${baseUrl}/users`);
+  // const data = await response.json();
+  // console.log(data);
 
   return (
-    <div className="mr-4 text-center text-4xl text-cyan-500 opacity-50">
+    <div className="mr-4 text-center text-4xl text-cyan-500">
+      <Header />
       <h1>Learn from the vast ocean of knowledge!</h1>
 
-      <p>{JSON.stringify(data ?? '')}</p>
-      <Header />
+      {/* <p>{JSON.stringify(data ?? '')}</p> */}
+      <div className="grid grid-cols-4">
+        <CourseList />
+      </div>
     </div>
   );
 }
